5494adf398e2e8ea4440b9ecdd7eac25afbae488
Ankit Jain <ankit.jain@deltatech.gg>
2025-05-28 19:07:42 +0530

    updated with permission for android 11 & above

--- src/android/CordovaInstalledAppPlugin.java
+++ src/android/CordovaInstalledAppPlugin.java
@@ -3,6 +3,8 @@ package cordova.plugins;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.cordova.CordovaPlugin;
@@ -15,33 +17,37 @@ import org.json.JSONException;
  * This class echoes a string called from JavaScript.
  */
 public class CordovaInstalledAppPlugin extends CordovaPlugin {
+    public CallbackContext callback = null;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getInstalledAppList")) {
             callback = callbackContext;
-            cordova.setActivityResultCallback(this);
-            String message = args.getString(0);
-            this.getInstalledAppList(message, callbackContext);
+            getInstalledAppList(action, callbackContext);
             return true;
         }
         return false;
     }
 
     private void getInstalledAppList(String message, CallbackContext callbackContext) {
-        try{
-            PackageManager pm = getPackageManager();
+      cordova.getThreadPool().execute(new Runnable() {
+        @Override
+        public void run() {
+          try {
+            PackageManager pm = cordova.getActivity().getPackageManager();
             List<PackageInfo> packages = pm.getInstalledPackages(0);
-            arrayList<String> installedApps = new ArrayList<>();
+            ArrayList<String> installedApps = new ArrayList<String>();
             for (PackageInfo packageInfo : packages) {
-                String appName = packageInfo.applicationInfo.loadLabel(pm).toString();
-                String packageName = packageInfo.packageName;
-                installedApps.add(appName + " (" + packageName + ")");
+              String appName = packageInfo.applicationInfo.loadLabel(pm).toString();
+              String packageName = packageInfo.packageName;
+              installedApps.add(appName + " (" + packageName + ")");
             }
-            callback.success(installedApps);
-        } catch (Exception e) {
+            callback.success(new JSONArray(installedApps));
+          } catch(Exception e) {
             Log.e("CordovaInstalledAppPlugin", "Error getting installed apps", e);
             callbackContext.error("Error getting installed apps: " + e.getMessage());
+          }
         }
+      });
     }
 }
